f4b18e591a5cb726281f5b20fc2db5a40132b1e3
dlgusgh4608 <dlgusgh4008@gmail.com>
2020-11-07 17:12:15 +0900

    prepare to deploy app

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const router = express.Router();
+const path = require('path');
 const PORT = process.env.PORT || 4000;
 const TodoService = require('./services/todo');
 
@@ -16,5 +17,15 @@ app.use(express.json());
 require('./controllers/todo')(router, todoService);
 
 app.use(router);
+
+const buildPath = path.resolve(__dirname, '../build');
+
+app.get('*.*', (req, res) => {
+  res.sendFile(path.join(buildPath, req.url));
+});
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
